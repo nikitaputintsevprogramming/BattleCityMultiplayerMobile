1b993da1eaeeb54d5e4bb24222ae1dac47491e89
nikitaputintsevprogramming <100438132+nikitaputintsevprogramming@users.noreply.github.com>
2023-08-27 14:47:00 +0300

    Player Health

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 
 public class Player : NetworkBehaviour //даем системе понять, что это сетевой объект
 {
+
+    [SyncVar(hook = nameof(SyncHealth))] //задаем метод, который будет выполняться при синхронизации переменной
+    int _SyncHealth;
     public int Health;
     public GameObject[] HealthIndicators;
 
     void Update()
     {
+        if (isOwned) //проверяем, есть ли у нас права изменять этот объект
+        {
+            if (Input.GetKeyDown(KeyCode.H)) //отнимаем у себя жизнь по нажатию клавиши H
+            {
+                if (isServer) //если мы являемся сервером, то переходим к непосредственному изменению переменной
+                    ChangeHealthValue(Health - 1);
+                else
+                    CmdChangeHealth(Health - 1); //в противном случае делаем на сервер запрос об изменении переменной
+            }
+        }
         PlayerMovement();
         UpdateHealthIndicators();
     }
@@ -33,12 +46,21 @@ public class Player : NetworkBehaviour //даем системе понять, что это сетевой об
         }
     }
 
-    [SyncVar(hook = nameof(SyncHealth))] //задаем метод, который будет выполняться при синхронизации переменной
-    int _SyncHealth;
-
     //метод не выполнится, если старое значение равно новому
     void SyncHealth(int oldValue, int newValue) //обязательно делаем два значения - старое и новое. 
     {
         Health = newValue;
+    }
+
+    [Server] //обозначаем, что этот метод будет вызываться и выполняться только на сервере
+    public void ChangeHealthValue(int newValue)
+    {
+        _SyncHealth = newValue;
+    }
+
+    [Command] //обозначаем, что этот метод должен будет выполняться на сервере по запросу клиента
+    public void CmdChangeHealth(int newValue) //обязательно ставим Cmd в начале названия метода
+    {
+        ChangeHealthValue(newValue); //переходим к непосредственному изменению переменной
     }
 }
